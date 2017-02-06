426d2bb40efeba43e0ee52f3d45259e64c37a9ea
Jonathan Cocks <joncocks@hotmail.com>
2017-02-06 11:52:16 -0800

    Set reply_to properly for emails from contact page

--- app/emails.py
+++ app/emails.py
@@ -8,6 +8,7 @@ def send_async_email(app, msg):
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
+    msg.reply_to = sender
     msg.body = text_body
     msg.html = html_body
     msg.reply_to = sender
